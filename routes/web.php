a240d7b380d0b9bf803af9b9176f0f6fdada8f2d
Onah Sunday <onally2005@gmail.com>
2020-06-22 22:56:21 +0100

    - Fix: update database
    Signed-off-by: Onah Sunday <onally2005@gmail.com>

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return response()->json([
-        'Team 048 - Teacher Recruitment Portal'
-    ]);
+    return response()->json('Team 048 - Teacher Recruitment Portal');
 });
